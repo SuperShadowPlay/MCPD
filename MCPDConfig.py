f4deaa24cb23d19ec21e39c9a8cc156f2cfb84f9
SuperShadowPlay <42284150+SuperShadowPlay@users.noreply.github.com>
2019-01-12 21:18:34 -0500

    Added runbot.py and MCPDConfig.py v1.1

---
+++ MCPDConfig.py
@@ -0,0 +1,34 @@
+#Config file for MCPD v1.1
+"KEEP THE QUOTES"
+"True and False and case sensitive!"
+#################################
+
+#Bot's run token
+TOKEN = "null"
+
+#Minecraft Server IP and Port (the IP *will* display to users, so capitalize at will
+cIP = "Insert IP here"
+cPort = 00000
+
+#Player count's refresh in seconds (Default = 60)
+cRefresh = 60
+
+#What message to use for the beginnings of commands, setting this to 0 makes it an @mention to the bot (Default 0)
+cBasePrompt = 0
+
+#"enable-query" in your "server.properties" MUST BE SET TO "true" TO DISPLAY PLAYER NAMES
+#Once that is done, set this to True to enable player names in mc! list (Default False)
+cEnableNames = False
+
+#What message to send with <prompt> list
+#{0} is replaced by the number of players online
+#{1} is replaced by the names of the online players - ONLY USE IF cEnableNames IS TRUE
+#{2} is replaced by the IP of the server
+cMessageSend = "{0} Player(s) Online"
+
+#If <prompt> list is called and there are no players
+#Set to true if you want to send above message anyway. Set false if you want a custom no players online message (Default False)
+cSkipNoPlayers = False
+#If above is set to False, set the message that will display instead
+#{0} is replace by the IP of the server
+cNoPlayers = "There are no players on {0}"
--- runbot.py
+++ runbot.py
@@ -1,57 +1,124 @@
-#By SuperShadowPlay#8793
+#MCPD v1.1 By SuperShadowPlay#8793 
 import discord
 import asyncio
 from mcstatus import MinecraftServer
 import datetime
 import time
-
+from MCPDConfig import *
 
 client = discord.Client()
 
-try:
-    my_file = open('psConfig.txt')
-except IOError:
-    print('Run configbot.py first!')
+'''Imports:
+TOKEN
+cIP
+cPort
+cRefresh
+cBasePrompt
+cEnableNames
+cSkipNoPlayers
+cNoPlayers'''
+
+print('#MCPD v1.1 By SuperShadowPlay#8793')
+
+#Checks for TOKEN
+if TOKEN == 'null':
+    print('Edit MCPDConfig.py and add your bot\'s run token')
+    print('and other settings!')
     input()
     quit()
-    
-#Read config data and turn it to a list
-def readConfig():
-    _readConfig = open("psConfig.txt", "r")
-    _returnedConfig = _readConfig.read().split(';')
-    _readConfig.close()
-    return _returnedConfig
-config = readConfig()
-#End read config data
+
+#Get time in H:M:S format
+def getTime():
+    _bigTime = time.strftime('%H:%M:%S')
+    return _bigTime
 
 #Bot status for player count
 async def playerSidebar():
     await client.wait_until_ready()
     while not client.is_closed:
-        mcServer = MinecraftServer(config[2], int(config[3]))
+        mcServer = MinecraftServer(cIP, cPort)
         serverStatus = mcServer.status()
+        if cEnableNames == True:
+            mcQuery = mcServer.query()
         sidebarCount = '{0} Players Online'.format(serverStatus.players.online)
-        print('Sidebar Updated To: ' + sidebarCount + ': ' + str(datetime.datetime.now()))
         await client.change_presence(game=discord.Game(name=sidebarCount))
-        await asyncio.sleep(int(config[4]))
-        #Change the player count on the basis of how many seconds were inputted into config[4]
+        await asyncio.sleep(int(cRefresh))
+        #Change the player count on the basis of how many seconds were inputted into cRefresh
 
 @client.event
 async def on_message(message):
+    msgSplit = message.content.split()
+
+    if len(msgSplit) == 1 or 0:
+        return
+    
     if message.author == client.user:
         return
 
-    if message.content == '<@' + str(client.user.id) + '>':
-        await client.send_message(message.channel, 'Pong!')
-        print('Pong\'ed user ' + str(message.author.id) + ' at ' + str(datetime.datetime.now()))
+    #Makes cBasePrompt = 0 usable
+    if cBasePrompt == 0:
+        cPrompt = '<@' + str(client.user.id) + '>'
+        
+    #Detects if the bot was called
+    if msgSplit[0] == cPrompt:
+        
+        #<prompt> ping - Pings the bot
+        if msgSplit[1].lower() == 'ping':
+            await client.send_message(message.channel, 'Pong!')
+            print('Pong\'ed user ' + str(message.author) + ' at ' + str(getTime()))
+
+        #<prompt> list - Lists players online. Only the amount is listed if cEnableNames is False
+        if msgSplit[1].lower() == 'list':
+            mcServer = MinecraftServer(cIP, cPort)
+            serverStatus = mcServer.status()
+
+            if serverStatus.players.online == 0:
+                if cSkipNoPlayers == False:
+                    await client.send_message(message.channel, cNoPlayers.format(cIP))
+                
+            if cEnableNames == True and '{1}' in cMessageSend:
+                mcQuery = mcServer.query()
+                await client.send_message(message.channel, cMessageSend.format(serverStatus.players.online, ", ".join(mcQuery.players.names), cIP))
+
+            else:
+                await client.send_message(message.channel, cMessageSend.format(serverStatus.players.online))
+
+async def printStatus():
+    #Prints the updating status to the console
+    await client.wait_until_ready()
+    while not client.is_closed:
+        mcServer = MinecraftServer(cIP, cPort)
+        serverStatus = mcServer.status()
+                    
+        if cEnableNames == True and '{1}' in cMessageSend and serverStatus.players.online != 0:
+            mcQuery = mcServer.query()
+            if serverStatus.players.online == 1:
+                print('{0} Player :: {2} :: {1}'.format(serverStatus.players.online, ", ".join(mcQuery.players.names), getTime()))
+            else:
+                print('{0} Players :: {2} :: {1}'.format(serverStatus.players.online, ", ".join(mcQuery.players.names), getTime()))
+
+        else:
+            if serverStatus.players.online == 1:
+                print('{0} Player :: {1}'.format(serverStatus.players.online, getTime()))
+            else:
+                print('{0} Players :: {1}'.format(serverStatus.players.online, getTime()))
+
+        await asyncio.sleep(int(cRefresh))
+        
 
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
+    #Enables correct display when cPrompt is 0
+    if cBasePrompt == 0:
+        cPromptText = '@' + client.user.name
+    else:
+        cPromptText = str(cPrompt)
+    print('Prompt: ' + cPromptText)
     print('------')
     
 client.loop.create_task(playerSidebar())
-TOKEN = config[1]
+client.loop.create_task(printStatus())
 client.run(TOKEN)
