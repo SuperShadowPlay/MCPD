3f463f5c55dec4ffa68ff3995872bf2b5dd9fce1
Trevor <supershadowplay@gmail.com>
2019-06-26 16:30:52 -0400

    pep8 and pyflakes are ruthless
    Since this is an older project for me,
    I decided to try to fix any stylisation errors
    BOY WAS THAT A BAD IDEA
    The way this program works is a whole lotta `if`
    statements and it means W0401 is going to be
    ignored from now on. I'll try to stay in-line
    but you can only indent so much in python.
    
    Hopefully whoever reads this is not too angry at me.

--- runbot.py
+++ runbot.py
@@ -1,12 +1,10 @@
-#MCPD v1.2 from github.com/SuperShadowPlay/MCPD
-print('Loading MCPD v1.2')
+"""MCPD v1.2 from github.com/SuperShadowPlay/MCPD ."""
 import discord
 import asyncio
 from mcstatus import MinecraftServer
-import datetime
 import time
 from MCPDConfig import *
-
+print("Loaded MCPD v1.2")
 client = discord.Client()
 
 '''Imports:
@@ -17,28 +15,32 @@ cRefresh
 cBasePrompt
 cEnableNames
 cSkipNoPlayers
-cNoPlayers'''
+cNoPlayers
+cEnableOutput
+cOutputChannel
+cDynamicOutput'''
 
-#Checks for TOKEN
+
 if TOKEN == 'null':
+    """Checks for TOKEN"""
     print('Edit MCPDConfig.py and add your bot\'s run token')
     print('and other settings!')
     input()
     quit()
 
-#Get time in H:M:S format
+
 def getTime():
+    """Get time in H:M:S format."""
     _bigTime = time.strftime('%H:%M:%S')
     return _bigTime
 
-#Bot status for player count
+
 async def playerSidebar():
+    """Bot status for player count."""
     await client.wait_until_ready()
     while not client.is_closed:
         mcServer = MinecraftServer(cIP, cPort)
         serverStatus = mcServer.status()
-        if cEnableNames == True:
-            mcQuery = mcServer.query()
         sidebarCount = '{0} Players Online'.format(serverStatus.players.online)
         await client.change_presence(game=discord.Game(name=sidebarCount))
         await asyncio.sleep(int(cRefresh))
@@ -46,9 +48,10 @@ async def playerSidebar():
 
 @client.event
 async def on_message(message):
+    """On message portion, most of the actual programming is in this function."""
     if message.author == client.user:
         return
-    
+
     msgSplit = message.content.split()
     #If the message content is **only** an image, this
     #prevents an error message from clogging the console.
@@ -62,38 +65,47 @@ async def on_message(message):
         cPrompt = '<@' + str(client.user.id) + '>'
     else:
         cPrompt = cBasePrompt
-        
+
     #Detects if the bot was called
     if msgSplit[0] == cPrompt:
 
         #<prompt> help - Lists commands
         if msgSplit[1].lower() == 'help':
-            await client.send_message(message.channel, '''The commands available are:
+            await client.send_message(message.channel,
+                                      '''The commands available are:
 {0} Help - Displays this message
 {0} List - List the players online at {1}
 {0} Ping - Ping the bot
 {0} Source - Github Source Code'''.format(cPrompt, cIP))
-        
+
         #<prompt> ping - Pings the bot
         if msgSplit[1].lower() == 'ping':
             await client.send_message(message.channel, 'Pong!')
-            print('Pong\'ed user ' + str(message.author) + ' :: ' + str(getTime()))
+            print('Pong\'ed user ' + str(message.author)
+                  + ' :: ' + str(getTime()))
 
-        #<prompt> list - Lists players online. Only the amount is listed if cEnableNames is False
+        #<prompt> list - Lists players online. Only the amount is
+        #listed if cEnableNames is False
         if msgSplit[1].lower() == 'list':
             mcServer = MinecraftServer(cIP, cPort)
             serverStatus = mcServer.status()
 
             if serverStatus.players.online == 0:
-                if cSkipNoPlayers == False:
+                if cSkipNoPlayers is False:
                     await client.send_message(message.channel, cNoPlayers.format(cIP))
-                
-            elif cEnableNames == True and '{1}' in cMessageSend and serverStatus.players.online != 0:
-                mcQuery = mcServer.query()
-                await client.send_message(message.channel, cMessageSend.format(serverStatus.players.online, ", ".join(mcQuery.players.names), cIP))
+
+            elif cEnableNames is True and '{1}' in cMessageSend:
+                if serverStatus.players.online != 0:
+                    onPlayers = serverStatus.players.online
+                    mcQuery = mcServer.query()
+                    await client.send_message(
+                                              message.channel,
+                                              cMessageSend.format(onPlayers,
+                                                                  ", ".join(mcQuery.players.names), cIP))
 
             else:
-                await client.send_message(message.channel, cMessageSend.format(serverStatus.players.online))
+                await client.send_message(message.channel,
+                                          cMessageSend.format(serverStatus.players.online))
 
         #<prompt> source - Github link
         if msgSplit[1].lower() == 'source':
@@ -102,14 +114,15 @@ Full source code at:
 https://github.com/SuperShadowPlay/MCPD''')
             print(str(message.author) + ' Requested Source :: ' + getTime())
 
+
 async def printStatus():
-    #Prints the updating status to the console
+    """Print the updating status to the console."""
     await client.wait_until_ready()
     while not client.is_closed:
         mcServer = MinecraftServer(cIP, cPort)
         serverStatus = mcServer.status()
-                    
-        if cEnableNames == True and '{1}' in cMessageSend and serverStatus.players.online != 0:
+
+        if cEnableNames is True and '{1}' in cMessageSend and serverStatus.players.online != 0:
             mcQuery = mcServer.query()
             if serverStatus.players.online == 1:
                 print('{0} Player :: {2} :: {1}'.format(serverStatus.players.online, ", ".join(mcQuery.players.names), getTime()))
@@ -123,10 +136,11 @@ async def printStatus():
                 print('{0} Players :: {1}'.format(serverStatus.players.online, getTime()))
 
         await asyncio.sleep(int(cRefresh))
-        
+
 
 @client.event
 async def on_ready():
+    """Log in and other such wonders."""
     print('Logged in as:')
     print(client.user.name)
     print(client.user.id)
@@ -137,7 +151,7 @@ async def on_ready():
         cPromptText = str(cBasePrompt)
     print('Prompt: ' + cPromptText)
     print('------')
-    
+
 client.loop.create_task(playerSidebar())
 client.loop.create_task(printStatus())
 client.run(TOKEN)
