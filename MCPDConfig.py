266cef33ca868b5cde7633e96c1258efcd71a027
Trevor <supershadowplay@gmail.com>
2020-01-08 15:40:46 -0500

    Added cChannels in config file

--- MCPDConfig.py
+++ MCPDConfig.py
@@ -16,5 +16,10 @@ cRefresh = 60
 #What message to use for the beginnings of commands, setting this to 0 makes it an @mention to the bot (Default 0)
 cBasePrompt = "0"
 
-#List of user ids to send a message to when the server is down:
+#List of ids to send a message to when the server is down:
+#Example line: cUsers = [111111111111111111, 123456789012345678, 112112112112122122]
+
+#Put user ids in here:
 cUsers = []
+#Put channel ids in here:
+cChannels = []
